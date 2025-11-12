442ca60c37fa142d7a645e2f83f35be20ab645ef
Natalia Diaz <nataliadiazg6@gmail.com>
2025-11-12 12:54:47 -0300

    Inicio paquete models- clase base

---
+++ TPIProgramacion/src/models/Base.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author Natalia
+ */
+public abstract class Base {
+    private int id;
+    private Boolean eliminado;
+}
