dc64a4c55932d890f914aa48bc6398cdfb9a6427
Nataliadiaz6 <nataliadiazg6@gmail.com>
2025-11-16 07:39:15 -0300

    Cambio nombres 02_datos_prueba.sql

--- sql/02_datos_prueba.sql
+++ sql/02_datos_prueba.sql
@@ -1,13 +1,13 @@
 USE tpi;
 
-INSERT INTO pedido (eliminado, numero, fecha, cliente_nombre, total, estado)
+INSERT INTO pedido (eliminado, numero, fecha, clienteNombre, total, estado)
 VALUES
 (0, 'P-0001', '2025-11-01', 'Juan Pérez',   15000.00, 'NUEVO'),
 (0, 'P-0002', '2025-11-02', 'María López',  23000.50, 'FACTURADO'),
 (0, 'P-0003', '2025-11-03', 'Carlos Gómez',  8000.00, 'ENVIADO');
 
 INSERT INTO envio (eliminado, tracking, empresa, tipo, costo,
-                   fecha_despacho, fecha_estimada, estado, pedido_id)
+                   fechaDespacho, fechaEstimada, estado, pedido.id)
 VALUES
 (0, 'TRK-AND-0001', 'ANDREANI', 'ESTANDAR', 1200.00,
  '2025-11-02', '2025-11-05', 'EN_TRANSITO', 1),
